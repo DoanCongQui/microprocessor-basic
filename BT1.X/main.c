9bc02992b904d28731fb7df4ae4ce2447086ffcb
IUH-Quis <92429927+DoanCongQui@users.noreply.github.com>
2025-02-12 22:42:02 +0700

    Update main.c

--- BT1.X/main.c
+++ BT1.X/main.c
@@ -1,16 +1,28 @@
-#include <xc.h>
+/*
+ * File:   main.c
+ * Author: dq
+ *
+ * Created on February 12, 2025, 9:48 PM
+ */
 
-#pragma config FOSC = HS        
-#pragma config WDTE = OFF       
-#pragma config PWRTE = OFF      
-#pragma config MCLRE = ON      
-#pragma config CP = OFF         
-#pragma config CPD = OFF       
-#pragma config BOREN = OFF      
-#pragma config IESO = OFF       
-#pragma config FCMEN = OFF      
-#pragma config LVP = OFF        
-#pragma config DEBUG = OFF     
+
+// PIC16F887 Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG1
+#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator: High-speed crystal/resonator on RA6/OSC2/CLKOUT and RA7/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = OFF      // RE3/MCLR pin function select bit (RE3/MCLR pin function is digital input, MCLR internally tied to VDD)
+#pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
+#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+#pragma config BOREN = OFF      // Brown Out Reset Selection bits (BOR disabled)
+#pragma config IESO = OFF       // Internal External Switchover bit (Internal/External Switchover mode is disabled)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
+#pragma config LVP = OFF        // Low Voltage Programming Enable bit (RB3 pin has digital I/O, HV on MCLR must be used for programming)  
+
+#include <xc.h>
 
 #define _XTAL_FREQ 4000000      //Thach Anh 4MHz
 
