f5adee95903067c2a3a7894a7d41587d284203c4
DoanCongQui <congt4644@gmail.com>
2025-02-16 16:17:09 +0700

    Update BT1.X

---
+++ BT1.X/led.h
@@ -0,0 +1,20 @@
+/* 
+ * File:   led.h
+ * Author: dq
+ *
+ * Created on February 16, 2025, 4:11 PM
+ */
+
+#ifndef LED_H
+#define	LED_H
+
+#include <xc.h>
+#define _XTAL_FREQ 4000000 
+void _8_LED();
+void LED_SANG_DUOI();
+void _8_LED_SANG_DUOI_BIT();
+void _16_LED_SANG_DUOI_ARRAY();
+void _16_LED_SANG_DUOI_BIT();
+
+#endif	/* LED_H */
+
